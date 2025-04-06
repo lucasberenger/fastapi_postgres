d7e851a73ded13d7916870c8b662f5f44a8fb538
lucasberenger <thegreatberenger@gmail.com>
2025-04-06 17:22:45 -0300

    fix expire date at create access token func

--- app/auth/auth_handler.py
+++ app/auth/auth_handler.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from jose import JWTError, jwt
 import os
 
@@ -8,7 +8,7 @@ ACCESS_TOKEN_EXPIRE_MINUTES=int(os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES'))
 
 def create_access_token(data: dict):
     to_encode = data.copy()
-    expire = datetime.now(datetime.zonetime.utc()) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
