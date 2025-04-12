e2a5bca7d977feba0900b36692386431df54f085
lucasberenger <thegreatberenger@gmail.com>
2025-04-11 22:11:49 -0300

    create dto to register a new user with his company

--- app/schemas/auth_dto.py
+++ app/schemas/auth_dto.py
@@ -1,4 +1,12 @@
 from pydantic import BaseModel, EmailStr
+from user_dto import UserCreateDTO
+from company_dto import CompanyCreateDTO
+
+
+class RegistrationDTO(BaseModel):
+    user: UserCreateDTO
+    company: CompanyCreateDTO
+
 
 class LoginDTO(BaseModel):
     email: EmailStr
