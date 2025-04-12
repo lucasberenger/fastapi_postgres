d764b76e0ce2c320e0724dada439fc936425506f
lucasberenger <thegreatberenger@gmail.com>
2025-04-12 10:56:31 -0300

    create base config to tests

---
+++ app/tests/conftest.py
@@ -0,0 +1,20 @@
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from core.database import Base
+from core.database import get_db
+
+TEST_DATABASE_URL='sqlite:///:memory:'
+
+engine = create_engine(TEST_DATABASE_URL, connect_args={"check_same_thread": False})
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+@pytest.fixture(scope="function")
+def db():
+    Base.metadata.create_all(bind=engine)
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+        Base.metadata.drop_all(bind=engine)
